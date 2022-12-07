b266553e1f6827e886654cf8095783ee05000ba4
FahimMontasir <fahimmontasirshakil@gmail.com>
2022-12-07 15:16:09 +0600

    setup prettier and eslint

--- rse-shop/frontend/tailwind.config.js
+++ rse-shop/frontend/tailwind.config.js
@@ -7,5 +7,13 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: false,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+  },
 };
