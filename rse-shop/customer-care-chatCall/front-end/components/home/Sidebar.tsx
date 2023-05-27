2dc75a357ffa0d941bf6cde4fd77f369114ae1ef
FahimMontasir <fahimmontasirshakil@gmail.com>
2023-05-27 16:31:15 +0600

    removed customer chat app into another repo

--- rse-shop/customer-care-chatCall/front-end/components/home/Sidebar.tsx
+++
@@ -1,9 +0,0 @@
-function Sidebar() {
-  return (
-    <aside className="sticky top-0 z-[9999] hidden w-[250px] bg-white-c dark:bg-black-c md:block md:h-screen md:flex-none">
-      all contacts
-    </aside>
-  );
-}
-
-export default Sidebar;
