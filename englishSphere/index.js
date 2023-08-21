918df057d1926683a1a871327bd894bf99ffd633
FahimMontasir <fahimmontasirshakil@gmail.com>
2023-08-21 14:06:43 +0600

    [start] migration

---
+++ englishSphere/index.js
@@ -0,0 +1,20 @@
+// import "./app/services/pushNotification"
+import React from "react"
+import { AppRegistry } from "react-native"
+import RNBootSplash from "react-native-bootsplash"
+import App from "./src/app"
+import { name as appName } from "./app.json"
+
+// Check if app was launched in the background and conditionally render null if so
+const HeadlessCheck = ({ isHeadless }) => {
+  if (isHeadless) {
+    // App has been launched in the background by iOS, ignore
+    return null
+  }
+
+  // Render the app component on foreground launch
+  return <App hideSplashScreen={RNBootSplash.hide} />
+}
+
+AppRegistry.registerComponent(appName, () => HeadlessCheck)
+// AppRegistry.registerComponent(appName, () => App)
---
+++ englishSphere/react-native.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  assets: [
+    // TODO: link documentation about fonts to this section
+  ],
+};
